5140d97b4dbef8c799e688941a4152306254b3c4
fepucinelli <felipepucinelli@gmail.com>
2026-03-10 22:33:47 -0300

    Updates

--- _assets/CX9klItk.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./C-vissj2.js";const t=a("/avatar.webp"),p=a("/avatar.jpg");export{t as _,p as a};
---
+++ _assets/Do2ktX_q.js
@@ -0,0 +1,1 @@
+import{p as a}from"./DlVNbyE9.js";const t=a("/avatar.webp"),p=a("/avatar.jpg");export{t as _,p as a};
